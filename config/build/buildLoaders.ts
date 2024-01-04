6fb33232605dfed37dade40f7537cf8e0112be77
Dmitry <ggbox@mail.ru>
2024-01-05 02:06:42 +0400

    added css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,7 +10,15 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
             // Creates `style` nodes from JS strings
             options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
-            "css-loader",
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.modules.')),
+                        localIdentName: options.isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                    },
+                }
+            },
             // Compiles Sass to CSS
             "sass-loader",
         ],
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,8 @@
-import React, {useState} from "react";
+import {useState} from "react";
+
+import classes from './Counter.modules.scss';
 import './counter.scss';
+
 
 export const Counter = () => {
     const [value, setValue] = useState(0)
@@ -12,10 +15,10 @@ export const Counter = () => {
         value === 0 ? null :
         setValue(value-1)
     }
-
+    
     return (
         <div className="counter">
-            <button onClick={decrement}>-</button>
+            <button className={classes.btn} onClick={decrement}>-</button>
             <span>{value}</span>
             <button onClick={increment}>+</button>
         </div>
