ca9947511ae84e8050189cd2b18a83a585b9d4dd
Dmitry <ggbox@mail.ru>
2024-01-05 04:06:31 +0400

    react route lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
+
 import "./index.scss";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import { Link } from "react-router-dom";
+import { HomePageAsync } from "./pages/HomePage/HomePage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from "react";
 
 
 export const App = () => {
     return (
         <div className="app">
-            dfgd
-            <Counter/>
+            <Link to={'/about'}>about</Link>
+            <Link to={'/'}>home</Link>
+            <Suspense fallback={<div>loading...</div>}>
+                <Routes>
+                    <Route path={'/'} element={<HomePageAsync />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     )
 };
