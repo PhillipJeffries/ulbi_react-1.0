9c200ae4833178970485e4dcd30f2c8aeaffc1e0
Dmitry <ggbox@mail.ru>
2024-01-13 22:01:35 +0400

    added sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -4,17 +4,20 @@ import { routeConfig } from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
     return (
-        <Suspense fallback={<div>loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({element, path}) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={element}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+        <div className="page-wrapper">
+            <Suspense fallback={<div>loading...</div>}>
+                <Routes>
+                    {Object.values(routeConfig).map(({ element, path }) => (
+                        <Route
+                            key={path}
+                            path={path}
+                            element={element}
+                        />
+                    ))}
+                </Routes>
+            </Suspense>
+        </div>
+
     )
 }
 
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,38 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from './Sidebar.modules.scss';
+import { FC, useState } from "react";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar: FC = ({ className }: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setCollapsed(prev => !prev)
+    }
+
+    return (
+        <div>
+            <div 
+                className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            >
+                <button className={cls.sidebarButton} onClick={onToggle}>
+                    x
+                </button>
+                <div className={cls.sidebarContent}>
+                    <ul>
+                        <li>eat</li>
+                        <li>shit</li>
+                    </ul>
+                    <div className={cls.switchers}>
+                        <ThemeSwitcher/>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
