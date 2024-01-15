fbb4ab28e7eb51c2bbf85e9d196443d248fcb761
Dmitry <ggbox@mail.ru>
2024-01-15 22:29:27 +0400

    i18next

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './app/styles/index.scss';
 
 import { App } from "./app/App";
 import { ThemeProvider } from './app/providers/ThemeProvider';
+import './shared/config/i18n/i18n';
 
 const root = createRoot(document.getElementById('root'));
 
